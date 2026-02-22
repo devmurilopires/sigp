18cd803917c41d6281ee77b9cbfc6ba8bebefdd1
devmurilopires <murilopiresoficial@gmail.com>
2026-02-22 13:55:57 -0300

    Adaptando o main a aba de parecer

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PIL import Image
 from src.auth.view import LoginView
 from src.auth.service import AuthService
 from src.ordem_servico.view import renderizar as renderizar_os
+from src.parecer.view import renderizar as renderizar_parecer
 
 try:
     from src.shared.utils import resource_path
@@ -79,7 +80,7 @@ def iniciar_sistema(usuario_dados):
 
     criar_placeholder(abas["Gráficos"], "Dashboard e Gráficos")
     criar_placeholder(abas["Relatórios OS"], "Relatórios de OS")
-    criar_placeholder(abas["Parecer Técnico"], "Geração de Parecer Técnico")
+    renderizar_parecer(abas["Parecer Técnico"], nome_usuario)
     criar_placeholder(abas["Relatórios Parecer"], "Relatórios de Pareceres")
     criar_placeholder(abas["Histórico"], "Lixeira e Histórico")
     if is_admin: criar_placeholder(abas["Cadastro de Endereço"], "Cadastro de Endereços")
