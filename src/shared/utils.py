e6d50d23e98465bf9ee6d0f072212be78b98ead2
Murilo de Sousa Pires <22719@etufor.ce.gov.br>
2026-02-25 11:27:58 -0300

    Refatorando o utils

--- src/shared/utils.py
+++ src/shared/utils.py
@@ -1,6 +1,7 @@
-# utils.py
+# shared/utils.py
 import sys
 import os
+import unicodedata
 
 def resource_path(relative_path):
     """
@@ -15,3 +16,13 @@ def resource_path(relative_path):
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
+
+def normalizar_texto(texto):
+    """
+    Remove acentos e deixa em caixa alta. 
+    Excelente para padronizar textos antes de salvar no banco de dados ou 
+    para cruzar dados em gráficos no Dashboard.
+    """
+    if not texto: return ""
+    nfkd = unicodedata.normalize('NFKD', str(texto))
+    return "".join(c for c in nfkd if not unicodedata.combining(c)).upper().strip()
